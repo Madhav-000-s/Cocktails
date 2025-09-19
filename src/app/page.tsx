f3555dbc7c57664ffba62c1e5acd7adbf305f235
madhav <madhavendranaths@gmail.com>
2025-09-19 17:37:10 +0530

    hero section and navbar implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import gsap from "gsap"
 import { ScrollTrigger,SplitText } from "gsap/all"
 gsap.registerPlugin(ScrollTrigger,SplitText)
+import Navbar from "@/components/Navbar"
+import Hero from "@/components/Hero"
 
 export default function main(){
-  return(<>Lets start!</>)
+  return(<main>
+            <Navbar/>
+            <Hero/>
+            <div className="h-dvh bg-black"/>
+         </main>)
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client"
+import gsap from "gsap"
+import { useGSAP } from '@gsap/react'
+import { navLinks } from '@/constants'
+
+const Navbar = () => {
+ useGSAP(() => {
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: 'rgba(0, 0, 0, 0)',
+	 backdropFilter: 'blur(30px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
+ 
+ return (
+	<nav className="fixed top-0 w-full z-50">
+	 <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+		<a href="#home" className="flex items-center gap-2 ">
+		 <img src="/images/logo.png" alt="logo" />
+		 <p>Velvet Pour</p>
+		</a>
+		
+		<ul>
+		 {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+		</ul>
+	 </div>
+	</nav>
+ )
+}
+export default Navbar
